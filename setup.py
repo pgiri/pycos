c8b039ce7d58550c31092c62afab93aacb229235
pgiri <pgiri@yahoo.com>
2017-10-31 23:13:12 -0400

    Updated version to 4.6.5.
    Updated version to 4.6.5.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='pycos',
-    version='4.6.4',
+    version='4.6.5',
     description='Concurrent, Asynchronous, Distributed, Communicating Tasks with Python',
     long_description=open('README.rst').read(),
     keywords='concurrency, asynchronous, network programming, distributed, tasks, message passing',
