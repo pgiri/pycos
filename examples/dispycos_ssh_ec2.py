6629a40b03311308739c76e17e73ba3c673b4106
pgiri <pgiri@yahoo.com>
2018-06-11 20:13:36 -0400

    Changed to new port numbers, fixed documentation.
    Ports now used are 9705 instead of 51347 etc. Fixed documentation
    to clarify steps to setup port forwarding.

--- examples/dispycos_ssh_ec2.py
+++ examples/dispycos_ssh_ec2.py
@@ -1,13 +1,13 @@
 # Run 'dispycosnode.py' program on Amazon EC2 cloud computing and run this
 # program on local computer.
 
-# in this example, ssh is used for port forwarding make sure EC2 instance allows
-# inbound TCP port 51347 (and any additional ports, depending on how many CPUs
-# are used by dispycosnode) assume '54.204.242.185' is external IP address of EC2
-# instance; login to that node with the PEM key: 'ssh -i my-key.pem
-# 4567:127.0.0.1:4567 54.204.242.185' run dispycosnode on EC2 at port (starting
-# with) 51347 with: 'dispycosnode.py -d --ext_ip_addr 54.204.242.185 --tcp_ports
-# 51347'
+# In this example ssh is used for port forwarding.  Make sure EC2 instance
+# allows inbound TCP port 9706 and any additional ports, depending on how many
+# CPUs are used by dispycosnode. Assume '54.204.242.185' is external IP address
+# of EC2 instance. Login to that node with remote port forwarding:
+# 'ssh -R # 9705:127.0.0.1:9705 54.204.242.185' (so port 9705, used by client is
+# forwarded), then run dispycosnode on EC2 server at port (starting with) 9706 with:
+# 'dispycosnode.py -d --ext_ip_addr 54.204.242.185 --tcp_ports 9706'
 
 # Distributed computing example where this client sends computation to remote
 # dispycos process to run as remote tasks. At any time at most one
@@ -25,10 +25,11 @@ from pycos.dispycos import *
 
 # this generator function is sent to remote dispycos servers to run
 # tasks there
-def compute(n, task=None):
-    import time
+def compute(i, n, task=None):
+    # 'i' is job number and 'n' is seconds to suspend task (to simulate
+    # computation time)
     yield task.sleep(n)
-    raise StopIteration(time.asctime()) # result of 'compute' is current time
+    raise StopIteration((i, n))
 
 
 def client_proc(computation, njobs, task=None):
@@ -39,19 +40,19 @@ def client_proc(computation, njobs, task=None):
         raise Exception('Could not schedule computation')
 
     # pair EC2 node with this client with:
-    yield pycos.Pycos().peer(pycos.Location('54.204.242.185', 51347))
+    yield pycos.Pycos().peer(pycos.Location('54.204.242.185', 9706))
     # if multiple nodes are used, 'broadcast' option can be used to pair with
     # all nodes with just one statement as:
-    # yield pycos.Pycos().peer(pycos.Location('54.204.242.185', 51347), broadcast=True)
+    # yield pycos.Pycos().peer(pycos.Location('54.204.242.185', 9706), broadcast=True)
 
     # execute n jobs (tasks) and get their results. Note that number of
     # jobs created can be more than number of server processes available; the
     # scheduler will use as many processes as necessary/available, running one
     # job at a server process
-    args = [random.uniform(3, 10) for _ in range(njobs)]
+    args = [(i, random.uniform(3, 10)) for i in range(njobs)]
     results = yield computation.run_results(compute, args)
     for result in results:
-        print('result: %s' % result)
+        print('job %s result: %s' % (result[0], result[1]))
 
     yield computation.close()
 
@@ -59,11 +60,11 @@ def client_proc(computation, njobs, task=None):
 if __name__ == '__main__':
     import sys, random
     # pycos.logger.setLevel(pycos.Logger.DEBUG)
-    pycos.Pycos(node='127.0.0.1', tcp_port=4567)
+    pycos.Pycos(node='127.0.0.1', tcp_port=9705, udp_port=9705)
     njobs = 10 if len(sys.argv) == 1 else int(sys.argv[1])
     # if scheduler is not already running (on a node as a program),
     # start private scheduler:
     Scheduler()
-    # send 'compute' generator function
+    # use 'compute' for computation jobs
     computation = Computation([compute])
     pycos.Task(client_proc, computation, njobs)
