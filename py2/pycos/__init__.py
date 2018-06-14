277ea15df7899c9f0978ec4998e6228b931886e9
pgiri <pgiri@yahoo.com>
2018-06-13 21:58:26 -0400

    Simplified Singleton metaclass.
    Changed Singleton metaclass so class instances are referred to
    using Singleton itself instead of given class 'cls', as seem to
    be general accepted practice. However, using 'cls' is confusing
    (at least not straigt forward and takes a bit of effort to have
    an idea of why it works). In any case, instances are never stored
    in 'cls' even if that is implied - it is just that object resolution
    eventually finds it back in Singleton class (unlike in the case
    when instances are stored with a variable and not a dictionary,
    as done in earlier versions of pycos, in which case the variable
    is stored in 'cls' when it is assigned).

--- py2/pycos/__init__.py
+++ py2/pycos/__init__.py
@@ -84,16 +84,18 @@ class Singleton(type):
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+        inst = Singleton._instances.get(cls, None)
+        if not inst:
+            inst = super(Singleton, cls).__call__(*args, **kwargs)
+            Singleton._instances[cls] = inst
+        return inst
 
     @classmethod
     def empty(_, cls):
         """
         Forget singleton instance.
         """
-        cls._instances.pop(cls, None)
+        Singleton._instances.pop(cls, None)
 
 
 class Logger(object):
