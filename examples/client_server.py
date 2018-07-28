a21525f832a0be3ec5eaa2bdfb2388622d877db5
pgiri <pgiri@yahoo.com>
2018-07-28 12:56:13 -0400

    Added client / server example file.

---
+++ examples/client_server.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# client and server tasks communicating with message passing
+# (asynchronous concurrent programming);
+# see https://pycos.sourceforge.io/pycos.html for details.
+
+import sys, random
+import pycos
+
+def server_proc(task=None):
+    task.set_daemon()
+    while True:
+        msg = yield task.receive()
+        print('Received %s' % (msg))
+
+msg_id = 0
+
+def client_proc(server, n, task=None):
+    global msg_id
+    for i in range(3):
+        yield task.suspend(random.uniform(0.5, 3))
+        # although multiple clients execute this method, locking is not
+        # necessary, as a task not preempted (unlike Python threads) and runs
+        # till 'yield'
+        msg_id += 1
+        server.send('msg_id %d: client %d, msg %d' % (msg_id, n, i))
+
+# create server
+server = pycos.Task(server_proc)
+# create 10 clients
+for i in range(10):
+    pycos.Task(client_proc, server, i)
