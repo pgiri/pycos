54a99ea058748497bce2de6346263d43616ca32a
pgiri <pgiri@yahoo.com>
2020-06-15 23:00:52 -0400

    Fixed syntax errors with Python 2.7
    In the last commit 'raise StopIteration(yield ...)' was used but
    apparently Python 2.7 doesn't accept this syntax; instead, now
    yield expressions are evaluated by surrounding parentheses as
    'raise StopIteration((yield ...))'.

--- py2/netpycos.py
+++ py2/netpycos.py
@@ -1557,7 +1557,7 @@ class SysTask(pycos.Task):
     def locate(name, location=None, timeout=None):
         if not SysTask._pycos:
             Pycos.instance()
-        raise StopIteration(yield Task._locate('~' + name, location, timeout))
+        raise StopIteration((yield Task._locate('~' + name, location, timeout)))
 
 
 class _NetRequest(object):
