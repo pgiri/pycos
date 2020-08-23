46399c614662f4174f34a64052cf9a71c3f80490
pgiri <pgiri@yahoo.com>
2020-08-23 13:05:45 -0400

    Did some of the suggested home work in dispycos_mas.py
    Removed channel, added periodic discovery of agents etc.

--- examples/dispycos_mas.py
+++ examples/dispycos_mas.py
@@ -1,14 +1,14 @@
 # Run 'dispycosnode.py' program on one or more nodes (to start servers to execute computations
 # sent by this client), and this program on local computer.
 
-# this is a simple example of multi-agent system (with a few caveats)
+# this is a simple example of multi-agent system
 
 import pycos.netpycos as pycos
 from pycos.dispycos import *
 
 
 # this generator function is sent to remote dispycos servers to run tasks (agents) there
-def agent_proc(channel, task=None):
+def agent_proc(client_task, task=None):
     import random
 
     # register this task so server peers can discover each other
@@ -21,16 +21,21 @@ def agent_proc(channel, task=None):
     def peer_status(task=None):
         task.set_daemon()
         while 1:
-            status = yield task.receive()
+            status = yield task.receive(timeout=60)
+            if not status:  # timed out
+                # periodically broadcast as UDP messages (used for discovery) may be lost
+                pycos_scheduler.discover_peers()
+                continue
             if isinstance(status, pycos.PeerStatus):
                 if status.status == pycos.PeerStatus.Online:
-                    pycos.logger.debug('%s: found peer %s', task.location, status.location)
+                    pycos.logger.debug('%s: found agent at %s', task.location, status.location)
                     # get reference to task (with same name) at that location
                     agent = pycos.Task.locate(task.name, timeout=3)
                     if isinstance(agent, pycos.Task):
                         agents.add(agent)
                 else:
-                    pycos.logger.debug('%s: peer %s disconnected', task.location, status.location)
+                    pycos.logger.debug('%s: agent at %s disconnected',
+                                       task.location, status.location)
                     for agent in agents:
                         if agent.location == status.location:
                             agent.discard(agent)
@@ -39,14 +44,9 @@ def agent_proc(channel, task=None):
     pycos_scheduler = pycos.Pycos.instance()
     # peer_status gets notifications of peers online and offline
     pycos_scheduler.peer_status(pycos.Task(peer_status))
-    # broadcast discover message for discovery; firewall must allow UDP on port 9706; if
-    # necessary, this can be done periodically
+    # broadcast discover messages to find other agents
     pycos_scheduler.discover_peers()
 
-    # channel running at client, as done here, is not good for multi-agent system, as its failure
-    # can break agents' ability to communicate; in this example channel is sometimes used to show
-    # latest updates to user (as well as to illustrate use cases for channels).
-    yield channel.subscribe(task)
     # in this simple case agents discover low and high valeues (silly exercise) randomly by
     # cooperating to accelerate the process
     low = high = random.uniform(1, 10000)
@@ -56,23 +56,22 @@ def agent_proc(channel, task=None):
         if msg:
             low = msg[1]
             high = msg[2]
-            continue
-        n = random.uniform(1, 10000)
-        if n < low:
-            low = n
-        elif n > high:
-            high = n
         else:
-            continue
+            n = random.uniform(1, 10000)
+            if n < low:
+                low = n
+            elif n > high:
+                high = n
+            else:
+                continue
 
-        if random.random() < 0.5:
-            channel.send((task, low, high))
-        elif agents:
-            # This agent can send to each known agent (in 'agents' captured with 'peer_status') if
-            # required (without using channel); here data is sent to one random agent
-            agent = random.choice(agents)
-            agent.send((task, low, high))
-
+        msg = (task, low, high)
+        # drop agents that may have gone away
+        drop = [agent for agent in agents if agent.send(msg)]
+        if drop:
+            for agent in drop:
+                agents.discard(agent)
+        client_task.send(msg)
 
 # status messages indicating nodes, servers and remote tasks finish status are sent to this local
 # task; in this case we process only servers initialized and closed
@@ -83,7 +82,7 @@ def status_proc(task=None):
         if isinstance(msg, DispycosStatus):
             if msg.status == Scheduler.ServerInitialized:
                 # start new agent at this server
-                agent = yield computation.run_at(msg.info, agent_proc, channel)
+                agent = yield computation.run_at(msg.info, agent_proc, client_task)
                 # there is no need to keep track of agents in this example, but done so here to
                 # show potential use
                 if isinstance(agent, pycos.Task):
@@ -95,25 +94,33 @@ def status_proc(task=None):
                         break
 
 
-# this local task submits client to dispycos scheduler, shows latest updates on channel
+# this local task submits client to dispycos scheduler, shows latest updates
 def client_proc(computation, task=None):
     # schedule computation with the scheduler
     if (yield computation.schedule()):
         raise Exception('schedule failed')
 
-    yield channel.subscribe(task)
+    update_from = None
     while 1:
-        msg = yield task.recv()
-        if not msg:  # from main to terminate
+        # as low / high values approach their limits, it may take long time to receive updates;
+        # here, the value are reset to middle if no update received for 60 seconds
+        msg = yield task.recv(timeout=60)
+        if not msg:
+            if update_from:
+                pycos.logger.info('Resetting values to 5000')
+                update_from.send((task, 5000, 5000))
+            continue
+        if msg == 'quit':
             break
-        # from channel with latest low / high values
+        # from an agent with latest low / high values
         pycos.logger.info('Update from %s: %.3f / %.3f ', msg[0].location, msg[1], msg[2])
+        update_from = msg[0]
 
     yield computation.close(terminate=True)
 
 
 if __name__ == '__main__':
-    import pycos.dispycos, sys, re
+    import pycos.dispycos, sys
     pycos.logger.setLevel(pycos.Logger.DEBUG)
     # if scheduler is not already running (on a node as a program), start it (private scheduler):
     Scheduler()
@@ -123,10 +130,8 @@ if __name__ == '__main__':
         }
     computation = Computation([agent_proc], **kwargs)
     agents = set()  # for illustration - not required in this example
-    channel = pycos.Channel('update_channel')
     client_task = pycos.Task(client_proc, computation)
-    print('   Enter "quit" or "exit" to end the program, or ')
-    print('   Enter one or two numbers separated by "," to reset low and high values')
+    print('   Enter "quit" or "exit" to end the program ')
     if sys.version_info.major > 2:
         read_input = input
     else:
@@ -139,20 +144,6 @@ if __name__ == '__main__':
             inp = read_input().strip().lower()
             if inp == 'quit' or inp == 'exit':
                 break
-            else:
-                try:
-                    m = re.match(r'\s*(\d+)[,/\s]*(\d*)', inp)
-                    low = float(m.group(1))
-                    if m.group(2):
-                        high = float(m.group(2))
-                    else:
-                        high = low
-                    inp = (low, high)
-                    # reset low and high for servers with given data (this is not in the spirit of
-                    # mulit-agent system behavior, but for illustration)
-                    channel.send((client_task, low, high))
-                except Exception:
-                    print('Invalid command "%s" ignored' % inp)
         except KeyboardInterrupt:
             break
-    client_task.send(None)
+    client_task.send('quit')
