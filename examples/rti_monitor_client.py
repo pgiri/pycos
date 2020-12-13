b6faed70cb2d2c1a43cd83136de9a487a25a777d
pgiri <pgiri@yahoo.com>
2020-12-13 09:42:32 -0500

    Improved documentation in RTI examples.
    Made RTI examples easier to understand.

--- examples/rti_monitor_client.py
+++ examples/rti_monitor_client.py
@@ -9,9 +9,9 @@ import pycos
 import pycos.netpycos
 
 
+# this task gets MonitorStatus messages (notifications of exit status) for (remote) tasks created
+# in rti_client
 def monitor_proc(n, task=None):
-    # this task gets exceptions (notifications of exit status) for (remote)
-    # tasks created in rti_test
     done = 0
     while done < n:
         msg = yield task.receive()
@@ -20,35 +20,37 @@ def monitor_proc(n, task=None):
             if msg.type == StopIteration:
                 pycos.logger.debug('RTI %s finished with: %s', rtask, msg.value)
             else:
-                pycos.logger.debug('RTI %s failed with: %s', rtask, msg.type)
+                pycos.logger.debug('RTI %s failed with: %s', rtask, msg.value)
             done += 1
         else:
             pycos.logger.warning('ignoring invalid message')
 
 
-def rti_test(task=None):
-    # if server is on remote network, automatic discovery won't work,
-    # so add it explicitly
+def rti_client(task=None):
+    # if server is on remote network, automatic discovery won't work, so add it explicitly
     # yield scheduler.peer(pycos.Location('192.168.21.5', 9705))
 
     # get reference to RTI at server
-    rti1 = yield pycos.RTI.locate('rti_1')
-    print('RTI is at %s' % rti1.location)
+    rti = yield pycos.RTI.locate('rti_server')
+    print('RTI is at %s' % rti.location)
 
-    # 5 (remote) tasks are created with rti1
+    # 5 (remote) tasks are created with rti
     n = 5
-    # set monitor (monitor_proc task) for tasks created for this RTI
-    yield rti1.monitor(pycos.Task(monitor_proc, n))
+    # set monitor (monitor_proc task) for tasks created for this RTI; even without monitor,
+    # exceptions are shown
+    rti.monitor(pycos.Task(monitor_proc, n))
 
     for i in range(n):
-        rtask = yield rti1('test%s' % i, b=i)
-        pycos.logger.debug('RTI %s created' % rtask)
-        # If necessary, each rtask can also be set (different) 'monitor'
+        # run task at server; tasks with odd number for 'b' will fail
+        rtask = yield rti('test%s' % i, b=i)
+        pycos.logger.debug('rtask %s created', rtask)
+        # messages can be exchanged with rtask; in this case, 'rti_server' expecpts to receive
         rtask.send('msg:%s' % i)
         yield task.sleep(random.uniform(0, 1))
 
 
-pycos.logger.setLevel(pycos.Logger.DEBUG)
-# use 'test' secret so peers that use same secret are recognized
-scheduler = pycos.Pycos(name='client', secret='test')
-pycos.Task(rti_test)
+if __name__ == '__main__':
+    pycos.logger.setLevel(pycos.Logger.DEBUG)
+    # use 'test' secret so peers that use same secret are recognized
+    scheduler = pycos.Pycos(name='client', secret='test')
+    pycos.Task(rti_client)
--- examples/rti_monitor_server.py
+++ examples/rti_monitor_server.py
@@ -2,10 +2,9 @@
 
 # server program for client sending requests to execute tasks
 
-# run this program and then client either on same node or different
-# node on local network. Server and client can also be run on two
-# different networks but client must call 'scheduler.peer' method
-# appropriately.
+# run this program and then client (rti_monitor_client.py) either on same node or different node
+# on local network. Server and client can also be run on two different networks but client must
+# call 'scheduler.peer' method appropriately.
 
 import sys
 import pycos
@@ -19,35 +18,39 @@ if sys.version_info.major == 3:
          (__file__, sys.version_info.major, sys.version_info.minor))
 
 
-def rti_1(a, b=1, task=None):
-    pycos.logger.debug('running %s/%s with %s, %s', task.name, id(task), a, b)
-    msg = yield task.receive()
-    if b % 2 == 0:
+# when client invokes RTI in this program, function below is used to start a task
+def rti_server(a, b=1, task=None):
+    pycos.logger.debug('running %s with %s, %s', task, a, b)
+    # receive message from client
+    msg = yield task.receive(timeout=2)
+    # to illustrate how client's monitor can receive exit values or exceptions, exception is
+    # raised if given b is not a positve number, otherwise task sleeps for b seconds and exits
+    # with msg
+    if isinstance(b, (int, float)) and b > 0 and isinstance(msg, str):
         yield task.sleep(b)
-        pycos.logger.debug('%s/%s done', task.name, id(task))
-        # (remote) monitor (if any) gets this exception (to be
-        # interpreted as normal termination)
+        # (remote) monitor (if any) gets back msg (to be interpreted as normal termination)
         raise StopIteration(msg)
     else:
-        # (remote) monitor (if any) gets this exception, too
+        # (remote) monitor (if any) gets this exception
         raise Exception('invalid invocation: %s' % b)
 
 
-pycos.logger.setLevel(pycos.Logger.DEBUG)
-# 'secret' is set so only peers that use same secret can communicate
-scheduler = pycos.Pycos(name='server', secret='test')
-# register rti_1 so remote clients can request execution
-rti1 = pycos.RTI(rti_1)
-rti1.register()
+if __name__ == '__main__':
+    pycos.logger.setLevel(pycos.Logger.DEBUG)
+    # 'secret' is set so only peers that use same secret can communicate
+    scheduler = pycos.Pycos(name='server', secret='test')
+    # register rti_server so remote clients can request execution
+    rti = pycos.RTI(rti_server)
+    rti.register()
 
-if sys.version_info.major > 2:
-    read_input = input
-else:
-    read_input = raw_input
-while True:
-    try:
-        line = read_input().strip().lower()
-        if line in ('quit', 'exit'):
+    if sys.version_info.major > 2:
+        read_input = input
+    else:
+        read_input = raw_input
+    while True:
+        try:
+            line = read_input().strip().lower()
+            if line in ('quit', 'exit'):
+                break
+        except Exception:
             break
-    except:
-        break
