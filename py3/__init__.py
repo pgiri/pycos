c5775a4eef4d58b06e90d4ecc8d1d748c42e6484
pgiri <pgiri@yahoo.com>
2022-09-25 15:43:10 -0400

    Optimization in task creation.
    'varargs' is much less likely to be used so check for the case
    where it is not used first.

--- py3/__init__.py
+++ py3/__init__.py
@@ -2889,11 +2889,12 @@ class Task(object):
             kwargs = kwargs.pop('kwargs', kwargs)
         if not inspect.isgeneratorfunction(target):
             raise Exception('%s is not a generator!' % target.__name__)
-        if target.__kwdefaults__ and target.__kwdefaults__.get('task', '') is None:
-            kwargs['task'] = task
-        elif (target.__defaults__ and
+        if (target.__defaults__ and
             'task' in target.__code__.co_varnames[:target.__code__.co_argcount][-len(target.__defaults__):]):
             kwargs['task'] = task
+        elif target.__kwdefaults__:
+            if target.__kwdefaults__.get('task', '') is None:
+                kwargs['task'] = task
         return target(*args, **kwargs)
 
     def __getstate__(self):
